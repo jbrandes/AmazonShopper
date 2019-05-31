2cfe07c794d0eda8eaeb3f3d057e08258b6c1653
Jordan Brandes <jordanbrandes@yahoo.com>
2019-05-31 13:07:22 -0500

    Create Shopping.py

---
+++ Shopping.py
@@ -0,0 +1,9 @@
+print("Would you like to go shopping today?")
+answer = input()
+
+if answer == "yes":
+    print("What should we get?")
+    list1 = input()
+    print("Great! We are going to get " + list1)
+if answer == "no":
+    print("Maybe tomorrow")
