96483533b02524f6545ac54b721842864bf99d9d
Jordan Brandes <jordanbrandes@yahoo.com>
2019-06-02 11:05:07 -0500

    More Functionality
    I made it so when the user types in their list a file is created with their list when the program is finished.

--- Shopping.py
+++ Shopping.py
@@ -1,9 +1,16 @@
 print("Would you like to go shopping today?")
-answer = input()
+
+answer = input() 
 
 if answer == "yes":
     print("What should we get?")
-    list1 = input()
+    list1 = input() 
     print("Great! We are going to get " + list1)
+    print("Your list is ready to go")
+    File_Object = open('shoppinglist.txt', 'w+')
+    File_Object.writelines(list1) 
+    File_Object.close()
+
+
 if answer == "no":
     print("Maybe tomorrow")
